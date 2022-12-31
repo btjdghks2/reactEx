dc5dd5c48354e419d50aaa769f48abeeab65d26f
btjdghks2 <btjdghks2@gmail.com>
2023-01-01 06:49:36 +0900

    hard

--- src/components_passvie/sidebardeco/Sidebar.jsx
+++ src/components_passvie/sidebardeco/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import './sidebar.css'
+
 
 export default class Sidebar extends Component {
   render() {
+    
+
+  
     return (
-      <div class='sidebar'>sidebar</div>
+      <div>sidebar</div>
+
       
     )
   }
