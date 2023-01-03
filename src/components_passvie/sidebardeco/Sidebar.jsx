842452d720cb1a262007c65ff4675233ba8dcd64
btjdghks2 <btjdghks2@gmail.com>
2023-01-03 18:23:37 +0900

    menu2

--- src/components_passvie/sidebardeco/Sidebar.jsx
+++ src/components_passvie/sidebardeco/Sidebar.jsx
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import "./Sidebar.css";
-
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default class Sidebar extends Component {
   render() {
-    
+    const Header = () => {
+
+      const [isOpen, setMenu] = useState(false);
+
+      const toggleMenu = () => {
+        setMenu(isOpen => !isOpen);
+      }
+    }
 
   
     return (
-      <div>
-        <input type = "checkbox " id="menuicon" />
-          <label for="menuicon">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
+      <div className="header">
+        <ul className="header-wrapper">
+          <li><MenuIcon onClick={()=>toggleMenu()}></MenuIcon></li>
+          <li><AiOutlineMenu/></li>
+          <li>
+            <Buttom
+              variant="contained"
+              color="secondary"
+              size="small"
+              style={buttonStyles}
+              >
+                
+              </Buttom>
+          </li>
+        </ul>
+        <ul className={isOpen ? "show-menu" : "hide-menu"}> 
+                        <li >1</li>
+                        <li >2</li>
+                        <li >3</li>
+                        <li >4</li>
+                </ul>
 
 
 
