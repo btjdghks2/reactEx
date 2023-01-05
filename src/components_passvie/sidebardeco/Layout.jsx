018d1940b11a7e5ee07f65b711c5fa0756436380
btjdghks2 <btjdghks2@gmail.com>
2023-01-05 20:00:05 +0900

    header_update

--- src/components_passvie/sidebardeco/Layout.jsx
+++ src/components_passvie/sidebardeco/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Layout.module.css'
-import { Head } from '../Head.jsx'
+
 import { Sidebar } from './Sidebar'
 
 
@@ -8,7 +8,7 @@ export const Layout = ({children}) => {
   return (
     <div className={styles.container}>
         {children}
-        <Head/>
+
         <div className={styles.layout}>
         <Sidebar/>
         <div className={styles.contents}>{children}</div>
